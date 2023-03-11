4fbe83160816e8b921a25dc447512678e68dc93c
gonzaloguaimas <gonzaloguaimas@gmail.com>
2023-03-11 01:07:30 -0300

    add dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTaskDTO } from './dto/task.dto';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() newTask: CreateTaskDTO) {
+    return this.tasksService.createTask(newTask.title, newTask.description);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { TaskStatus, Task } from './task.entity';
+import { v4 } from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'titulo',
+      description: 'Some Task',
+      status: TaskStatus.OPEN,
+    },
+  ];
+  getAllTasks() {
+    return this.tasks;
+  }
+  createTask(title: string, description: string) {
+    const newTask: Task = {
+      id: v4(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(newTask);
+    return newTask;
+  }
+  deleteTask() {}
+  updateTask() {}
+}
