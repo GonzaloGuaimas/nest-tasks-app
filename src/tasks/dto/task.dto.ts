ea95ce26fc0bee17f7cfa443781e6adf646c54ef
gonzaloguaimas <gonzaloguaimas@gmail.com>
2023-03-11 18:37:23 -0300

    add nest validator

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,29 @@
 /* eslint-disable prettier/prettier */
 import { TaskStatus } from '../task.entity';
+import { IsString, IsNotEmpty, MinLength, IsIn, IsOptional } from 'class-validator'
 export class CreateTaskDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   description: string;
 }
 
 export class UpdateTaskDTO {
+  @IsString()
+  @IsOptional()
   title?: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsString()
+  @IsOptional()
+  @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.OPEN])
   status?: TaskStatus
 }
